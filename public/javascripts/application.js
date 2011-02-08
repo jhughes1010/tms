7412b35032bbdb1760b5cb2297b6cba4461586aa
James Hughes <jhughes1010@gmail.com>
2011-02-08 04:28:10 -0700

    I believe all date fields are corrected and will no longer swap day and month

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
-  $("#task_tcd").datepicker();
+  $("#task_tcd").datepicker({ dateFormat: 'yy-mm-dd' });
+  $("#task_scd").datepicker({ dateFormat: 'yy-mm-dd' });
+  $("#task_acd").datepicker({ dateFormat: 'yy-mm-dd' });
 });
