5de85ade3aba53bc621204b71ec90c7aff7ff715
James Hughes <jhughes1010@gmail.com>
2011-02-18 20:15:35 -0700

    Testing PGSQL error

--- app/models/pto.rb
+++ app/models/pto.rb
@@ -5,6 +5,6 @@ class Pto < ActiveRecord::Base
   
   def self.upcoming_range(st,fi)
     #find(:all, :order =>"start,user_id", :conditions => ["(start >= ? OR end >= ?) AND start < ?", st, st, fi])
-    find(:all, :order =>"start,user_id", :conditions => ["(start >= ?", st])
+    find(:all, :order =>"start,user_id", :conditions => ["start >= ?", st])
   end
 end
