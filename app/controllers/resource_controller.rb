db0aadd8c49cef8ba445cf84e3e1f55832b2b07f
James Hughes <jhughes1010@gmail.com>
2011-05-18 05:34:21 -0600

    WIP - Impport CSV routine

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -7,26 +7,28 @@ class ResourceController < ApplicationController
     header = 0
     #filenames
     import_file = "import/test_engineering.csv"
-    #Get forecast data
-    #f=File.open(import_file)
-    #text = f.readlines
-    #f.close
     puts
     puts "============================================="
     puts "CSV importer for Resource Allocation database"
     puts "============================================="
     #import and stack data   
+    #delete database
     arr_of_arrs = FasterCSV.read(import_file) 
     arr_of_arrs.each do |x|  
       stack_data(x) if header == 1
       header = 1 if header == 0
     end
   end 
+  #
+  #
+  #
   def stack_data(line)
     #puts line
     write_record(line)
   end
-
+  #
+  #
+  #
   def write_record(record)
     #write new record to database
     4.upto(24){
@@ -34,6 +36,7 @@ class ResourceController < ApplicationController
       date = Date.today
       date_start = date.at_beginning_of_month - 3.months
       print date_start
+      print " "
       0.upto(3){
         |y|
         print record[y] + " "
@@ -42,16 +45,19 @@ class ResourceController < ApplicationController
           print ":Actual   "
       else
           print ":Forecast "
-          date_current=date_start.months_since(x)
+          date_current=date_start.months_since(x-4)
        new_to_db(date_current,record[0], record[1],record[2],record[3],record[x])  
             end
       puts record[x]
 
     }
   end
+  #
+  #
+  #
   def new_to_db(date,dept,name,project,function,time)
-    #delete database
     print date
+    print " "
     
   end
 end
