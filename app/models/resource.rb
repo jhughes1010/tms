71f58f39139bdec564bf2ae50e363c2c41c18d07
James Hughes <jhughes1010@gmail.com>
2011-06-17 05:15:24 -0600

    WIP

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,6 +4,7 @@ class Resource < ActiveRecord::Base
     find(:all, :conditions =>["project = 'Sapphire' AND date >= ? AND date < ?" , date, date +15.months], :order =>"date", :select => "sum (forecast) as forecast", :group => "date")
   end
   def self.sapphire_sub(date)
-    find(:all, :conditions =>["project = 'Sapphire' AND date >= ? AND date < ?" , date, date +15.months], :order =>"date", :select => "sum (forecast) as forecast", :group => "date")
+    #find(:all, :conditions =>["project = 'Sapphire' AND date >= ? AND date < ?" , date, date +15.months], :order =>"date", :select => "sum (forecast) as forecast", :group => "department, date")
+    self.find(:all).where("project = 'Sapphire' AND date >= ? AND date < ?" , date, date +15.months)
   end
 end
