0a55458d7c479580708ea2f6890ba31a1a0dd2a3
James Hughes <jhughes1010@gmail.com>
2011-07-22 05:44:37 -0600

    more SQL errors

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -7,7 +7,7 @@ class Resource < ActiveRecord::Base
     self.where("project = ? AND date >= ? AND date < ?" ,project, date, date +15.months).order("date")
   end
   def self.project_detail_name(date,project)
-    self.where("project = ? AND date >= ? AND date < ?" ,project, date, date +15.months).order("date").group("project", "department", "name", "function").order("department","name","function").select("department, name, function, project")
+    self.where("project = ? AND date >= ? AND date < ?" ,project, date, date +15.months).order("date").group("project", "department", "name", "function").order("department","name","function").select("department, name, function, project, date")
   end
   def self.department_total(date,project,department)
     self.where("project = ? AND department LIKE ? AND date >= ? AND date < ?" ,project, "%#{department}%", date, date +15.months).order("date").select("sum (forecast) as forecast").group("date")
