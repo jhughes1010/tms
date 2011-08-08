cb8da28eceb34dc2b6b233ffb8d43f4d4545cff6
James Hughes <jhughes1010@gmail.com>
2011-08-08 05:42:54 -0600

    WIP:CAN import mains

--- app/controllers/can_controller.rb
+++ app/controllers/can_controller.rb
@@ -53,19 +53,3 @@ class CanController < ApplicationController
     r.save
   end
 end
-
-
-t.string   "can"
-t.boolean  "internal"
-t.boolean  "sort"
-t.boolean  "mark"
-t.boolean  "ft"
-t.boolean  "assy_loc"
-t.boolean  "device_tab"
-t.boolean  "inactive"
-t.string   "customer"
-t.string   "interface"
-t.string   "mfg_id"
-t.string   "i2w_addr"
-t.text     "comments"
-t.string   "fw_version"
