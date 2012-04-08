596a738d430894df21ca35d2adbad8abc07b6b5f
James Hughes <jhughes1010@gmail.com>
2012-04-08 14:28:12 -0600

    pull calculated fields into class methods

--- app/models/costing.rb
+++ app/models/costing.rb
@@ -1,4 +1,6 @@
 class Costing < ActiveRecord::Base
+  attr_accessor :untested_die_cost, :die_sort_cost, :die_assembly_cost
+
   def self.search(params)
     results = scoped
     results = results.where("device like ?", "%" + params[:device] + "%") if params[:device]
@@ -10,5 +12,45 @@ class Costing < ActiveRecord::Base
 
   def self.scope_builder
     DynamicDelegator.new(scoped)
+  end
+
+  def untested_die_cost
+    self.wafercost/self.netgooddie
+  end
+  def untested_die_cost_percentage
+    self.untested_die_cost/self.total_cost
+  end
+  def die_sort_cost
+    self.diecost - self.untested_die_cost
+  end
+  def die_sort_cost_percentage
+    self.die_sort_cost/self.total_cost
+  end
+  def die_assembly_cost
+    self.asm_cost - self.diecost
+  end
+  def die_assembly_cost_percentage
+    self.asm_cost - self.diecost
+  end
+  def die_ft_cost
+    self.tst_cost - self.asm_cost
+  end
+  def die_ft_cost_percentage
+    self.tst_cost - self.asm_cost
+  end
+  def die_backend_cost
+    self.fg_subcon
+  end
+  def die_backend_cost_percentage
+    self.fg_subcon
+  end
+  def die_overhead_cost
+    self.tst_ohcost + self.asm_ohcost
+  end
+  def die_overhead_cost_percentage
+    self.tst_ohcost + self.asm_ohcost
+  end
+  def total_cost
+    self.fg_stdcost
   end
 end
