e322e46ad47baf60bf4f6d95329b69913654c636
James Hughes <jhughes1010@gmail.com>
2012-07-22 06:10:43 -0600

    date_received column in test_progs change to date field

---
+++ db/migrate/20120722120755_rename_test_prog_date_field_type.rb
@@ -0,0 +1,9 @@
+class RenameTestProgDateFieldType < ActiveRecord::Migration
+  def self.up
+    change_column test_progs: :date_received, :date
+  end
+
+  def self.down
+    change_column test_progs: :date_received, :string
+  end
+end
