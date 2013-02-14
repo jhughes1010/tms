7f3e4ddc93451ac6392d99c166f64c7a4d82a32f
James Hughes <jhughes1010@gmail.com>
2013-02-14 04:40:41 -0700

    BootStrap 2.0 CSS

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,6 @@
 class Target < ActiveRecord::Base
+  before_save :programs_uppercase
+  
   def self.defaults( family )
     t = self.where("family in (?) AND device = 'default'", family).limit(1)
   end
@@ -10,5 +12,17 @@ class Target < ActiveRecord::Base
   end
   def self.sorted
     t = self.order("family, device, tab")
+  end
+  private
+  def programs_uppercase
+    self.mav_cp1.upcase!
+    self.mav_cp2.upcase!
+    self.mav_cp3.upcase!
+    self.mag_cp1.upcase!
+    self.mag_cp2.upcase!
+    self.mag_cp3.upcase!
+    self.mag_x64_cp1.upcase!
+    self.mag_x64_cp2.upcase!
+    self.mag_x64_cp3.upcase!    
   end
 end
