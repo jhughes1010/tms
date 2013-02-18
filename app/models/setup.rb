faeb7dafb35ff98e47ca1afb2b9436bfb0d0f4ba
James Hughes <jhughes1010@gmail.com>
2013-02-18 11:28:41 -0700

    Finalized UI for a bit

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -1,7 +1,7 @@
 class Setup < ActiveRecord::Base
   def self.get_setups_magnum(part)
     
-    t = self.where("device in (?) AND platform NOT IN ('EPRO','dualEPRO','quadEPRO')", part).order("device, platform, tab,  location ")
+    t = self.where("device in (?) AND platform NOT IN ('EPRO','dualEPRO','quadEPRO')", part).order("device, tab, platform,  location ")
     #t = self.where("device in (?)", part).order("device, platform , tab,  location ")
   end
 end
