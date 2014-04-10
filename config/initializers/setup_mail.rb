ecc2e2a35b7a316991acddb058e399be975062c1
James Hughes <jhughes1010@gmail.com>
2014-04-10 04:44:06 -0600

    WIP: Mailer experiments with new server

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,18 @@
+#ActionMailer::Base.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :domain               => "atmel.com",
+#  :user_name            => "jhughes.atmel",
+#  :password             => "keyboard2012",
+#  :authentication       => "plain",
+#  :enable_starttls_auto => true
+#}
+
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "atmel.com",
-  :user_name            => "jhughes.atmel",
-  :password             => "keyboard2012",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :address => "ussmtp01.atmel.com",
+  :port => 25, # default is 25 but you can use also 26
+  :domain => 'atmel.com',
+  :user_name => "mem_bu_tools@atmel.com"
 }
 
 ActionMailer::Base.default_url_options[:host] = "10.95.114.17:3000"
