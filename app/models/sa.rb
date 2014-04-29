394d90fa0aecab13f8123675933f8d3bf25454e9
James Hughes <jhughes1010@gmail.com>
2014-04-29 05:24:21 -0600

    WIP SAS entry

--- app/models/sa.rb
+++ app/models/sa.rb
@@ -1,14 +1,38 @@
 class Sa < ActiveRecord::Base
   def self.mask_set(entry)
     mask = Hash.new
-    mask.default = true
     if entry == "Abnormal"
-      mask = {lot_number: true}
+      mask = {lot_number: true,
+        location: true,
+        profit_center: true,
+        sap_matid: true,
+        lts_matid: true,
+        quantity: true,
+        date: true,
+        created_at: true,
+        updated_at: true,
+        sas_type: true,
+        sent: true,
+        comment: true
+      }
     elsif entry == "Obsolete"
-      mask = {lot_number: true}
+      mask = {lot_number: true,
+        location: true,
+        plant: true,
+        profit_center: true,
+        sap_matid: true,
+        lts_matid: true,
+        quantity: true,
+        date: true,
+        created_at: true,
+        updated_at: true,
+        sas_type: true,
+        sent: true,
+        comment: true
+      }
     else
       mask.default = true
     end
     mask
   end
-end
+end  
