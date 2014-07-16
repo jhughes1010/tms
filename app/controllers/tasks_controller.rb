8528973ee46a9d925503f2f92ff7cae4a42552dd
James Hughes <jhughes1010@gmail.com>
2014-07-16 13:05:01 -0600

    don't send edit emails to James

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,8 +44,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     #deliver the mail
-    UserMailer.task_edit_confirmation(session[:user_id],@task).deliver
-    UserMailer.task_edit_confirmation(1,@task).deliver
+    unless user_id == 1
+      UserMailer.task_edit_confirmation(session[:user_id],@task).deliver
+      UserMailer.task_edit_confirmation(1,@task).deliver
+    end
   end
 
   # GET /tasks/1/duplicate
