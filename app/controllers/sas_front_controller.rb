478520478ed36b55005dcd35001f673fcf5b8348
James Hughes <jhughes1010@gmail.com>
2014-08-28 13:45:34 -0600

    WIP email works

--- app/controllers/sas_front_controller.rb
+++ app/controllers/sas_front_controller.rb
@@ -19,7 +19,7 @@ class SasFrontController < ApplicationController
     end
     write_records( @uploaded_io)
     #send_mail( recipient, file)
-    UserMailer.sas_mail(['james.hughes@atmel.com','james.hughes@atmel.com'], @uploaded_io.original_filename).deliver
+    UserMailer.scrap(['james.hughes@atmel.com', 'lisa.dougan@atmel.com', 'jeff.leasure@atmel.com'], @uploaded_io.original_filename).deliver
     
   end
   def send_mail( recipient, file)
