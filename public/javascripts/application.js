d4a693780ab668310963a2b6cdf02198d0f3d988
James Hughes <jhughes1010@gmail.com>
2014-11-12 10:14:57 -0700

    WIP MANID sample tables added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 jQuery(document).ready(function() {
+    jQuery("#sample_dateExchange").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+    jQuery("#sample_dateSamples").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+    jQuery("#sample_dateCustApproval").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+    jQuery("#sample_dateQAEnable").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+    jQuery("#sample_dateReleaseProduction").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+
   jQuery("#task_tcd").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
   jQuery("#task_scd").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
   jQuery("#task_acd").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
@@ -11,6 +17,7 @@ jQuery(document).ready(function() {
 
   jQuery("#pto_start").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
   jQuery("#pto_finish").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+  
   jQuery("#project_dr1").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
   jQuery("#project_dr2").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
   jQuery("#project_dr3").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
