ee5c41ca99d8da80b0158506c0e3b84834c201de
James Hughes <jhughes1010@gmail.com>
2016-02-23 04:28:03 -0700

    RNG controller cleanup

--- app/controllers/rng_controller.rb
+++ app/controllers/rng_controller.rb
@@ -6,34 +6,30 @@ class RngController < ApplicationController
   require 'timeout'
 
   def index
-
-
-
     @IPHash = {"10.95.111.200" => "Pass","10.95.111.201" => "Pass", "10.95.111.202" => "Pass", "10.95.111.203" => "Pass", "10.95.111.41" => "Pass" } 
     @IPHash.each {|k,v|
-    s = UDPSocket.new
-    s.connect(k, 10001)
-
-    s.send("rng", 0)
-      
-    (Timeout.timeout(1) {response, remote = s.recvfrom(100)}) rescue @passFlag = "Fail"
-    unless @passFlag == "Fail"
-      parts = response.split(",")
-
-      if parts[0] == '1'
-        puts parts[1]
-      @passFlag = "Pass"
-      else
-        puts "Error generating random number"
-        @passFlag = "Fail"
+      s = UDPSocket.new
+      s.connect(k, 10001)
+      s.send("rng", 0)
+      @passFlag="Pass"
+      response='0,unassigned' 
+      begin
+        status = Timeout::timeout(3) {response, remote = s.recvfrom(100)}
+      rescue 
+        @passFlag = "Fail - No Response - timeout failure"
       end
-    else
-      @passFlag = "Fail"
-    end
-
-    s.close
-    @IPHash[k] = @passFlag
-  }
-end
-
+      if @passFlag == "Pass"
+	      parts = response.split(",")
+        if parts[0] == '1'
+          puts parts[1]
+          @passFlag = "Pass"
+        else
+          puts "Error generating random number"
+          @passFlag = "Fail - RNG error code, no valid number. Please researt Pi and server."
+        end
+      end
+      s.close
+      @IPHash[k] = @passFlag
+    }
+  end
 end
